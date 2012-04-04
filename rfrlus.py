9735d6db31da2e6c638b5d92f1122bc03c9b24be
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-04-04 04:11:01 -0400

    Add notes about API, and remove login requirement.

--- rfrlus.py
+++ rfrlus.py
@@ -10,32 +10,32 @@ class KeyVal(db.Model):
     sval = db.StringProperty() 
     ival = db.IntegerProperty() 
 
+# Keys are the exposed methods.
+#fcnDict = {'r': ref, 'add': add, 'addJs': addJs, 'altAdd': altAdd, 'createlink.php': createlink, 'name': name, 'nameJS': nameJS}
+
+
 class MainPage(webapp.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            self.response.headers['Content-Type'] = 'text/html'
+        self.response.headers['Content-Type'] = 'text/html'
 
-            vals = KeyVal.gql("where keyStr='count' limit 1")
-            val=None
-            for v in vals:
-                val = v
+        vals = KeyVal.gql("where keyStr='count' limit 1")
+        val=None
+        for v in vals:
+            val = v
 
-            if val==None:
-                val = KeyVal()
-                val.keyStr='count'
-                val.ival=repr(1)
+        if val==None:
+            val = KeyVal()
+            val.keyStr='count'
+            val.ival=repr(1)
 
-            try:
-                v = int(val.ival)
-            except:
-                v=1
+        try:
+            v = int(val.ival)
+        except:
+            v=1
 
-            self.response.out.write("<html><body><h1>rfrl.us</h1> <p>Hello " + user.nickname() + " " + repr(v) + "</p></body></html>")
-            val.ival = v+ 1
-            val.put()
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        self.response.out.write("<html><body><h1>rfrl.us</h1> <p>Hello " + user.nickname() + " " + repr(v) + "</p></body></html>")
+        val.ival = v+ 1
+        val.put()
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
