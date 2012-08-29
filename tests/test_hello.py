72d3f2f79b4e687589ea6a465188f9e6ca954468
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-08-29 00:43:57 -0400

    Work on more tests

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -7,3 +7,18 @@ app = TestApp(application)
 def test_index():
     response = app.get('/hello')
     assert 'Hello' in str(response)
+
+#def test_name():
+#    pass
+
+def test_add_blank():
+    try:
+        resp = app.post('/add', {'url': ''})
+    except Exception:
+        pass
+    else:
+        assert False, "Blank URLs shouldn't be accepted."
+
+def test_add():
+    resp = app.post('/add', {'r': 'http://bobsays.com'})
+
